fb3ddbb51e762d0869d01139ab79fd19f077efd2
Kaushik373 <kaushik123tellakula@gmail.com>
2025-01-09 17:31:55 +0530

    datatypes content added

---
+++ 01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "kaushik@gmail.com"
+var accountPassword = "12345678"
+accountCity = "Hyderabad"
+let accountState
+accountEmail = "kt@gmail.com"
+accountPassword = "123123123"
+accountCity = "TamilNadu"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //to make the js use newer version rules on the code(for syntax)
+
+//alert(3) only used for the JS in the web
+
+console.log(3 + 3)
+
+console.log("Kaushik")
+
+let name = "Kaushik"
+let age = 21
+let isLoggedIn = true
+let state = null
+
+
+//number -> 2 to power 53
+//bigint
+//string -> ""
+//boolean -> ture/false
+//null - standalone value
+//undefined - undefined value of a variable
+//symbol - uniqueness to find out the component, used in react 
+
+//object ***
+console.log(typeof(isLoggedIn))
+//gives the datatype of the variables, but for null - it is null and undefined - undefined itselfx
