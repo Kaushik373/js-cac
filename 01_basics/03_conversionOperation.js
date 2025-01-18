df765f35c512890e6795d855c6440044237898cb
Kaushik373 <kaushik123tellakula@gmail.com>
2025-01-18 19:20:17 +0530

    Operations content added

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,3 +20,15 @@ console.log(valueInNumber);
     - “something” - true
 - conversion for string is String()
 */
+
+//**************Operations***************************
+console.log("\nOperations")
+let val = 3
+let negVal = -val
+console.log(negVal)
+
+// let str1 = "hello"
+// let str2 = " world"
+// console.log(str1+str2)
+
+console.log(2+2+"1"+2+2);
